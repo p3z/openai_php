90e2a99e7ef229772de1ae99bf62d2931826f4ac
Shay <chenille.pottle@yahoo.co.uk>
2024-01-01 15:47:07 +0000

    Functionalise each approach

--- index.php
+++ index.php
@@ -22,44 +22,136 @@ $data = [
     'messages' => [
         ['role' => 'user', 'content' => 'Describe the purpose of json to someone who is computer illiterate, do so in verbosely poetic old english.']
     ],
-    'temperature' => 0.7,
+    'temperature' => 1,
     //"max_tokens" => 2000
 ];
 
-$options = [
-    'http' => [
-        'header' => "Content-type: application/json\r\n" .
-                    "Authorization: Bearer " . $api_key,
-        'method' => 'POST',
-        'content' => json_encode($data),
-    ],
-];
+function send_http_req($api_url, $data, $api_key){
 
-$context = stream_context_create($options);
-$response = file_get_contents($api_url, false, $context);
+    $return = new stdClass();
 
-if ($response === false) {
-    // Handle error
-    echo 'Error accessing the API.';
-} else {
-        
-    $response_data = json_decode($response, true); 
-//     echo "<pre>";
-//     print_r($response_data);
-//     echo "</pre>";
-    $chat_response = $response_data['choices'][0]['message']['content'] ?? NULL;
+    $options = [
+        'http' => [
+            'header' => "Content-type: application/json\r\n" .
+                        "Authorization: Bearer " . $api_key,
+            'method' => 'POST',
+            'content' => json_encode($data),
+        ],
+    ];
 
-    if(!is_null($chat_response)){
-        echo $chat_response;
+    $context = stream_context_create($options);
+    $response = file_get_contents($api_url, false, $context);
 
-        /**
-         * $chat_response: Pray, lend me thine ear, for I shall unfold the tale of JSON in the parlance of old English so thou may comprehend its purpose, though thy knowledge of computers be frail...etc
-         * **/
-        
+    if ($response === false) {
+        // Handle error
+       // Handle error and get additional details
+        $meta = stream_get_meta_data($context);
+        //echo 'Error accessing the API: ' . implode("\n", $meta['wrapper_data']);
+
+        $return->status = false;
+        $return->message = 'Error accessing the API: ' . implode("\n", $meta['wrapper_data']);
     } else {
-        echo "Didnt work, troubleshooting time...";
+            
+        $response_data = json_decode($response, true); 
+    //     echo "<pre>";
+    //     print_r($response_data);
+    //     echo "</pre>";
+        $chat_response = $response_data['choices'][0]['message']['content'] ?? NULL;
+
+        if(!is_null($chat_response)){
+            //echo $chat_response;
+            
+                // echo "<pre>";
+                // print_r($chat_response);
+                // echo "</pre>";
+
+            /**
+             * $chat_response: Pray, lend me thine ear, for I shall unfold the tale of JSON in the parlance of old English so thou may comprehend its purpose, though thy knowledge of computers be frail...etc
+             * **/
+
+            $return->status = true;
+            $return->message = $chat_response;
+
+            
+        } else {
+            
+            $return->status = false;
+            $return->message = 'Query successful, but problem with parsing response.';
+        }
+
+        return $return;
+    } // end response received check
+
+
+
+} // end fn
+
+
+function send_curl_req($api_url, $data, $api_key) {
+    
+    $return = new stdClass();
+     
+     
+    $options = [
+        CURLOPT_URL => $api_url,
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_POST => true,
+        CURLOPT_POSTFIELDS => json_encode($data),
+        CURLOPT_HTTPHEADER => [
+            'Content-Type: application/json',
+            'Authorization: Bearer ' . $api_key,
+        ],
+        CURLOPT_SSL_VERIFYPEER => true,
+        CURLOPT_SSL_VERIFYHOST => 2,
+    ];
+
+    $curl = curl_init();
+    curl_setopt_array($curl, $options);
+
+    $response = curl_exec($curl);
+
+    if ($response === false) {
+        //echo 'cURL Error: ' . curl_error($curl);
+        
+        $return->status = false;
+        $return->message = 'cURL Error: ' . curl_error($curl);
+    } else {
+        
+       $response_data = json_decode($response, true); 
+        
+       $chat_response = $response_data['choices'][0]['message']['content'] ?? NULL;
+
+        if(!is_null($chat_response)){
+            //echo $chat_response;
+            
+                // echo "<pre>";
+                // print_r($chat_response);
+                // echo "</pre>";
+
+            /**
+             * $chat_response: Pray, lend me thine ear, for I shall unfold the tale of JSON in the parlance of old English so thou may comprehend its purpose, though thy knowledge of computers be frail...etc
+             * **/
+
+            $return->status = true;
+            $return->message = $chat_response;
+
+            
+        } else {
+            
+            $return->status = false;
+            $return->message = 'Query successful, but problem with parsing response.';
+        }
+            
+       
     }
 
-}
+     curl_close($curl);
+      return $return;
+} // end fn
+
+  
+ 
+
+
 
 ?>
